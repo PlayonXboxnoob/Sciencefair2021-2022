1148d7f8ffa6c51a2d532e6c16f41f6e44236086
PlayonXboxnoob <62848052+PlayonXboxnoob@users.noreply.github.com>
2022-02-21 17:10:59 -0500

    added comments

--- Verify.py
+++ Verify.py
@@ -1,9 +1,11 @@
 
-def verify(unsortedarr, verifylist):
+# function to verify if the sortedList is correct
+def verify(unsortedarr, sortedList):
 
-    unsortedarr.sort()
+    unsortedarr.sort()  # using in-built sorting to sort the list
 
-    for i in range(len(verifylist)):
-        if verifylist[i] != unsortedarr[i]:
-            return False
-    return True
+    # for each item in the two lists verify that the numbers are same
+    for i in range(len(sortedList)):
+        if sortedList[i] != unsortedarr[i]: 
+            return False    # Failed, the sorted list is not correct
+    return True # Success, sorted list is correct!
