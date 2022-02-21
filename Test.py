4991a321e3e6a10c834348a700aa1dd22d32c953
PlayonXboxnoob <62848052+PlayonXboxnoob@users.noreply.github.com>
2022-02-21 17:25:25 -0500

    comments

--- Test.py
+++ Test.py
@@ -11,6 +11,7 @@ from quickSort import *
 from insertionSort import *
 from Verify import *
 
+# dictionary of sorting methods and associated program
 alglist = {
     'quick': quickSort,
     'comb': combSort,
@@ -19,9 +20,11 @@ alglist = {
     'insertion': insertionSort
 }
 
+# list of tests, basically contains the amount of numbers and their lower and upper limits
 testList = [[1, 10000, 1000], [1, 10000, 5000], [1, 100000, 10000], [1, 1000000, 50000], [1, 10000000, 100000],
             [1, 1000000, 200000], [1, 1000000, 300000]]
 
+# number of times to run a sorting program, so we can average the time
 testCount = 10
 
 def runTest(totest, testList):
@@ -30,29 +33,29 @@ def runTest(totest, testList):
     timeTakenList = []
     numOpList = []
 
-    for testToRun in testList:
+    for testToRun in testList:  # Run the sorting method for a given list of numbers
         unsortedArray = RandN(testToRun[0], testToRun[1], testToRun[2])
 
         print("working in " + totest + 'sort for ' + str(testToRun[2]))
         print(datetime.now())
         sortedArray = unsortedArray.copy()
         start = time.time()
-        sortedArray, numOp = alglist[totest](sortedArray)
+        sortedArray, numOp = alglist[totest](sortedArray)   # run the sorting algorithm
         end = time.time()
         print("DONE " + totest + 'sort for ' + str(testToRun[2]))
         print(datetime.now())
 
-        if not verify(unsortedArray, sortedArray):
+        if not verify(unsortedArray, sortedArray):  #if my program failed!
             print('error in ' + totest + 'sort for ' + str(testToRun[2]))
             break
         else:
-            timeTaken = round((end-start), 4)
+            timeTaken = round((end-start), 4)   # save the time taken to sort this list
             timeTakenList.append(timeTaken)
-            numOpList.append(numOp)
+            numOpList.append(numOp)             # save number of comparisons required
             print('it took ' + str(timeTaken) + ' seconds')
             print('it took ' + str(numOp) + ' operations')
 
-    return timeTakenList, numOpList
+    return timeTakenList, numOpList     # return final list of time and comparisons
 
 if __name__ == '__main__':
 
