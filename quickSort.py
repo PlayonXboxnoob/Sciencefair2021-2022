e08f8f682589e4dcdea28afc437f3d6a7de0e754
PlayonXboxnoob <62848052+PlayonXboxnoob@users.noreply.github.com>
2022-02-21 16:56:40 -0500

    added comments

--- quickSort.py
+++ quickSort.py
@@ -8,32 +8,35 @@ from Verify import *
 global numOperations
 numOperations = 0
 
+# Outer quicksort function, it is used only to calculate
+# number of operations
 def quickSort(arr):
     global numOperations
     numOperations = 0
     return quickSortInner(arr), numOperations
 
+# Inner quicksort function, actual code
 def quickSortInner(arr):
     global numOperations
     n = len(arr)
-    if n < 1 or n == 1:
+    if n < 1 or n == 1: # If this is array of size 1 then return
         return arr
     else:
-        mid = arr.pop(int(n/2))
+        mid = arr.pop(int(n/2)) # else take the middle number
 
     higher = []
     lower = []
 
-    for num in arr:
+    # pivot the array into lower and higher arrays around middle number
+    for num in arr: 
         if num > mid:
             higher.append(num)
             numOperations += 1
         else:
             lower.append(num)
             numOperations += 1
-
-
-
+            
+    # recursively call the same function until done
     return quickSortInner(lower) + [mid] + quickSortInner(higher)
 
 if __name__ == '__main__':
